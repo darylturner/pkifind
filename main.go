0b0f1e60053d241494e6892944a08a044068d4a4
Daryl Turner <dturner@west.com>
2017-08-14 14:40:32 +0100

    slight refactor

--- main.go
+++ main.go
@@ -25,92 +25,87 @@ type match struct {
 	Expired    bool      `json:"expired"`
 }
 
-func getCRL(ca *string, client *vaultapi.Logical) (cl *pkix.CertificateList, err error) {
-	// get the pem encoded crl
-	crlResults, err := client.Read(fmt.Sprintf("%v/cert/crl", *ca))
+func getCRL(ca *string, client *vaultapi.Client) (c *pkix.CertificateList, err error) {
+	r, err := client.Logical().Read(fmt.Sprintf("%v/cert/crl", *ca))
+	if r == nil {
+		return c, fmt.Errorf("no value found at %v/cert/crl", *ca)
+	}
 	if err != nil {
 		return
 	}
 
-	// get the crl pem block from the api results
-	crlBlock, _ := pem.Decode([]byte(crlResults.Data["certificate"].(string)))
-	if crlBlock == nil {
-		return cl, errors.New("failed to decode crl")
+	crl, _ := pem.Decode([]byte(r.Data["certificate"].(string)))
+	if crl == nil {
+		return c, errors.New("failed to decode crl")
 	}
 
-	// decode the x509 into native certificate list
-	return x509.ParseCRL(crlBlock.Bytes)
+	return x509.ParseCRL(crl.Bytes)
 }
 
-func decodeCertificate(c string) (dc *x509.Certificate, err error) {
-	// get the cert pem block from the api results
+func decodeCertificate(c string) (d *x509.Certificate, err error) {
 	block, _ := pem.Decode([]byte(c))
 	if block == nil {
-		return dc, errors.New("failed to parse certificate")
+		return d, errors.New("failed to parse certificate")
 	}
 
-	// decode the x509 into native cert
 	return x509.ParseCertificate(block.Bytes)
 }
 
 func main() {
-	vault := flag.String("vault", "http://localhost:8200", "address of vault api")
-	ca := flag.String("ca", "", "vault pki mount to search through")
-	term := flag.String("s", "", "common name search term")
-	token := flag.String("token", "", "vault api token")
-
-	// process command line arguments
+	ca := flag.String("ca", "pki", "vault pki mount to search through")
+	term := flag.String("search", "", "common name search term")
+	addr := flag.String("address", "", "override VAULT_ADDR environment variable")
+	token := flag.String("token", "", "override VAULT_TOKEN environment variable")
 	flag.Parse()
-	if *ca == "" {
-		log.Fatal("please specify vault pki mount to search")
-	}
-	if *token == "" {
-		var ok bool
-		*token, ok = os.LookupEnv("VAULT_TOKEN")
-		if !ok {
-			log.Fatal("please supply vault token")
-		}
+
+	config := vaultapi.DefaultConfig()
+	config.ReadEnvironment()
+	if *addr != "" {
+		config.Address = *addr
 	}
 
-	// connect and set up authentication to vault
-	vc, err := vaultapi.NewClient(&vaultapi.Config{Address: *vault})
-	if err != nil {
-		log.Fatalf("error connecting to vault: %v\n", err)
-	}
-	vc.SetToken(*token)
-	logical := vc.Logical()
-
-	// retrieve decoded crl from vault api
-	crl, err := getCRL(ca, logical)
+	vc, err := vaultapi.NewClient(config)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	// get slice of all issued certificate serial numbers
-	certResults, err := logical.List(fmt.Sprintf("%v/certs", *ca))
+	if *token != "" {
+		vc.SetToken(*token)
+	}
+	if _, err := vc.Auth().Token().LookupSelf(); err != nil {
+		log.Fatal(err)
+	}
+
+	crl, err := getCRL(ca, vc)
 	if err != nil {
 		log.Fatal(err)
 	}
-	allCerts := certResults.Data["keys"].([]interface{})
 
-	// initialize slice of matched certificates to store results
+	r, err := vc.Logical().List(fmt.Sprintf("%v/certs", *ca))
+	if r == nil {
+		log.Fatal(fmt.Errorf("no value found at %v/certs", *ca))
+	}
+	if err != nil {
+		log.Fatal(err)
+	}
+	allCerts := r.Data["keys"].([]interface{})
+
 	res := make([]match, 0)
-
-	// range over serial numbers
 	for _, serial := range allCerts {
-		r, err := logical.Read(fmt.Sprintf("%v/cert/%v", *ca, serial)) // get raw certificate from vault
+		r, err := vc.Logical().Read(fmt.Sprintf("%v/cert/%v", *ca, serial))
+		if r == nil {
+			log.Fatal(fmt.Errorf("no value found at %v/cert/%v", *ca, serial))
+		}
 		if err != nil {
 			log.Fatal(err)
 		}
 
-		decoded, err := decodeCertificate(r.Data["certificate"].(string)) // decode the certificate
+		decoded, err := decodeCertificate(r.Data["certificate"].(string))
 		if err != nil {
 			log.Fatal(err)
 		}
 
-		// check to see if the search term is in the common name
 		if strings.Contains(decoded.Subject.CommonName, *term) {
-			// check to see if the serial number matches any in the revoked list
 			rev := false
 			for _, rc := range crl.TBSCertList.RevokedCertificates {
 				if decoded.SerialNumber.String() == rc.SerialNumber.String() {
@@ -119,7 +114,6 @@ func main() {
 				}
 			}
 
-			// push the matched onto the results slice
 			res = append(res, match{
 				CommonName: decoded.Subject.CommonName,
 				ValidFrom:  decoded.NotBefore,
@@ -131,7 +125,6 @@ func main() {
 		}
 	}
 
-	// encode the results as json to stdout
 	jsonOut := json.NewEncoder(os.Stdout)
 	jsonOut.SetIndent("", "  ")
 	if err := jsonOut.Encode(res); err != nil {
